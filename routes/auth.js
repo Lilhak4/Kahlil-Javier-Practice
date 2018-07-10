cb90ce3cf1e68fe51fc7e1268b3720eea915141b
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-07-10 20:33:03 +0200

    What we started

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+// -------Need to create user model
+
+const User = require('../models/user');
+
+router.get('/signup', (req, res, next) => {
+  if (req.session.currentUser) {
+    res.redirect('/');
+  }
+
+  res.render('auth/signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  if (req.session.currentUser) {
+    res.redirect('/');
+  }
+  if (!req.body.username || !req.body.password) {
+    res.redirect('auth/signup');
+  }
+  User.findOne({username: req.body.username})
+    .then((user) => {
+      if (user) {
+        return res.redirect('auth/signup');
+      }
+      // ------This is where the encryption goes
+    });
+});
